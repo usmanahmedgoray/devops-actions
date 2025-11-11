f12e25c2b8fc488c72b5f13cb3dc2a773d1d87b4
usmanahmedgoray <usmanahmedkharal@gmail.com>
2025-11-11 11:59:52 +0500

    First commit

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Usman Ahmed Goraya");
+console.log("GitHub action pipeline is setup");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Statting tetsts....");
+setTimeout(() => console.log('Waiting 3 seconds...'), 3000)
+console.log("tests complete....");
